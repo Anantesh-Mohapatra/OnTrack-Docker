9abad223c44444cabfb12725d4da7cdbee2357a0
Anantesh Mohapatra <ananteshmohapatra@gmail.com>
2024-12-21 14:43:01 -0500

    Station copy/paste messages

--- src/components/TrainSchedule.js
+++ src/components/TrainSchedule.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import '../styles/TrainSchedule.css'; // New import for TrainSchedule styles
 
+const roundUpToNearestMinute = (time) => {
+  const date = new Date(time);
+  if (date.getSeconds() > 0) {
+    date.setMinutes(date.getMinutes() + 1);
+  }
+  date.setSeconds(0, 0);
+  let hours = date.getHours();
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const ampm = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  return `${hours}:${minutes} ${ampm}`;
+};
+
+const copyToClipboard = (trainNumber, station, arrivalTime, isFirstStation) => {
+  const roundedTime = roundUpToNearestMinute(arrivalTime);
+  const action = isFirstStation ? 'leaving' : 'reaching';
+  const message = `I am on Train ${trainNumber}, ${action} ${station} at ${roundedTime}. You can see my train status at https://ontrack-551821400291.us-central1.run.app/`;
+  navigator.clipboard.writeText(message).then(() => {
+    // No need to set state for hover effect
+  });
+};
+
 const TrainSchedule = ({ trainData, isTrainActive, nextStop, formatTime, getStopStatus }) => {
   return (
     <div className="TrainSchedule">
@@ -29,7 +52,12 @@ const TrainSchedule = ({ trainData, isTrainActive, nextStop, formatTime, getStop
                   : ''
               }
             >
-              <td className="cell">{stop.STATIONNAME || 'N/A'}</td>
+              <td 
+                className="cell"
+                onClick={() => copyToClipboard(trainData.TRAIN_ID, stop.STATIONNAME, stop.TIME, index === 0)}
+              >
+                {stop.STATIONNAME || 'N/A'}
+              </td>
               <td className="cell">{formatTime(stop.TIME)}</td>
               <td className="cell">{formatTime(stop.DEP_TIME)}</td>
               <td
