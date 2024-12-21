6789453f9211ff1eba019ca30aa942d6639aac3c
Anantesh Mohapatra <ananteshmohapatra@gmail.com>
2024-12-20 20:11:00 -0500

    Separated styling from .js files Pt.2

--- src/App.js
+++ src/App.js
@@ -20,11 +20,11 @@ const App = () => {
   };
 
   return (
-    <div style={styles.container}>
-      <div style={styles.titleContainer}>
-        <div style={styles.strip}></div>
-        <h1 style={styles.title}>OnTrack</h1>
-        <div style={styles.strip}></div>
+    <div className="container">
+      <div className="titleContainer">
+        <div className="strip"></div>
+        <h1 className="title">OnTrack</h1>
+        <div className="strip"></div>
       </div>
       
       {/* Add the PopularTrains component */}
@@ -36,52 +36,6 @@ const App = () => {
       {!selectedTrain && <div className="helpButton" onClick={toggleHelpOverlay}>?</div>}
     </div>
   );
-};
-
-// Styles for the overall layout
-const styles = {
-  container: {
-    padding: '20px',
-    margin: '0 auto',
-    maxWidth: '800px',
-  },
-  titleContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: '20px',
-  },
-  title: {
-    fontFamily: 'Arial, sans-serif',
-    fontStyle: 'italic',
-    fontSize: '2em',
-    margin: '0 10px',
-  },
-  strip: {
-    width: '50px',
-    height: '15px',
-    background: 'linear-gradient(45deg, #f0803d, #b31b90, #014c97)',
-    transform: 'skewX(-20deg)',
-  },
-  helpButton: {
-    position: 'fixed',
-    bottom: '20px',
-    right: '20px',
-    width: '40px',
-    height: '40px',
-    borderRadius: '50%',
-    backgroundColor: '#f0803d',
-    color: '#fff',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: '1.5em',
-    cursor: 'pointer',
-    transition: 'background-color 0.3s',
-  },
-  helpButtonHover: {
-    backgroundColor: '#b31b90',
-  },
 };
 
 export default App;
